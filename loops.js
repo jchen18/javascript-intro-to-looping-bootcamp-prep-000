6ce834748cd7b6f74237c0b41333f23a1b0ef275
Jessie Chen <jessiechen0513@gmail.com>
2018-01-12 15:09:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,7 +14,7 @@ function whileLoop(number){
 
 function doWhileLoop(array){
   do {
-    maybeTrue()  
+    maybeTrue() = true 
 } while (array.length > 0 && maybeTrue());
   array.shift()
   return array
